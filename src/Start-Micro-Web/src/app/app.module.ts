6717e24be8b3587a762e67ec31e106e4336bb13f
hrichi <hrichi.mohamed@gmail.com>
2019-12-29 14:55:20 +0100

    use angular material

--- src/Start-Micro-Web/src/app/app.module.ts
+++ src/Start-Micro-Web/src/app/app.module.ts
@@ -4,6 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import {
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  MatButtonModule
+} from '@angular/material';
+
+
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'weather', component: WeatherComponent }
+];
 
 @NgModule({
   declarations: [
@@ -11,8 +30,23 @@ import { WeatherComponent } from './weather/weather.component';
     WeatherComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {
+        enableTracing: false
+      }
+    ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatListModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
