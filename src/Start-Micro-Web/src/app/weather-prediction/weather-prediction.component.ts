e9afd50ff7583e4989a3cb96fda392a4134aeb19
Hrichi Mohamed <hrichi.mohamed@hotmail.fr>
2020-01-23 21:15:46 +0100

    clean some code

--- src/Start-Micro-Web/src/app/weather-prediction/weather-prediction.component.ts
+++ src/Start-Micro-Web/src/app/weather-prediction/weather-prediction.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { WeatherPredictionService } from '../Services/weather-prediction.service'
 
 @Component({
